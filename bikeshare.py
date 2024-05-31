399a8c9cc819d59477b70c42b0be007b00f0ed84
trollin <anthony.rollin@outlook.com>
2024-05-31 14:57:46 -0400

    refactor bikeshare file adding turtles loop

---
+++ bikeshare.py
@@ -0,0 +1,13 @@
+# placeholder turtle progam
+# did not create the bikeshare program originally
+# b/c not enrolled from previous course mentioned to add
+
+import turtle
+
+aaron = turtle.Turtle()
+aaron.color("red")
+for side in [1, 2, 3, 4]:
+    aaron.forward(100)
+    aaron.right(90)
+
+#comment
